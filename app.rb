ae218f84fbeb5b1e1fff87becb3b3fb58adf2970
Amanda Vuong (she/her) <81902165+mandyvuong@users.noreply.github.com>
2021-09-08 09:11:35 +0100

    entering_players

--- app.rb
+++ app.rb
@@ -7,8 +7,13 @@ class Battle < Sinatra::Base
   end
 
   get '/' do
-    'Hello Battle!'
-    "Testing infrastructure working!"
+    erb :index
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
   end
 
   run! if app_file == $0
