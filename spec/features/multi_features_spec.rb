1e49a6ebde417860a45622b5a0008c6d073cb42d
Amanda Vuong (she/her) <81902165+mandyvuong@users.noreply.github.com>
2021-09-09 16:34:32 +0100

    Test Helpers - DRY

--- spec/features/multi_features_spec.rb
+++ spec/features/multi_features_spec.rb
@@ -1,19 +1,13 @@
 feature 'Enter names', :type => :feature do
   scenario 'submitting names' do
-    visit '/'
-    fill_in(:player_1_name, with: 'Jon')
-    fill_in(:player_2_name, with: 'Bob')
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_text('Jon vs. Bob')
   end
 end
 
 feature 'View hit points', :type => :feature do
   scenario 'see Player 2 hit points' do
-    visit '/'
-    fill_in(:player_1_name, with: 'Jon')
-    fill_in(:player_2_name, with: 'Bob')
-    click_button 'Submit'
+    sign_in_and_play
     save_and_open_page
     expect(page).to have_content 'Bob: 60HP'
   end
